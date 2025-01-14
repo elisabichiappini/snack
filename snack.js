0e63b2094e3e096ba779c71fddb0de634b3565d2
Elisa Chiappini <elisabichiappini@gmail.com>
2025-01-14 10:38:19 +0100

    feat: ok server collegato a porta e host

--- snack.js
+++ snack.js
@@ -6,10 +6,13 @@ dotenv.config();
 
 //import modulo server
 const http = require("http");
-const port = process.env.PORT || 'ciao port';
-const host = process.env.HOST || 'ciao host';
-http.createServer((res, req) => {
-    res.end();
-}).listen((port, host, () => {
+const port = process.env.PORT || 8889;
+const host = process.env.HOST || localhost;
+http.createServer((req, res,) => {
+    res.writeHead(200, {
+        "Content-type":"text/html; charset=utf-8"
+    });
+    res.end('<h1>ciao sono collegato</h1>');
+}).listen(port, host, () => {
     console.log(`server avviato su http://${host}:${port}`);
-}));
+})
