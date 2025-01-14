8e27d9cec62a206662c9c5183f7be991c58ec28d
Elisa Chiappini <elisabichiappini@gmail.com>
2025-01-14 10:18:47 +0100

    feat: server go

--- snack.js
+++ snack.js
@@ -1,1 +1,15 @@
 console.log('ciao');
+//import modulo .env
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+//import modulo server
+const http = require("http");
+const port = process.env.PORT || 'ciao port';
+const host = process.env.HOST || 'ciao host';
+http.createServer((res, req) => {
+    res.end();
+}).listen((port, host, () => {
+    console.log(`server avviato su http://${host}:${port}`);
+}));
