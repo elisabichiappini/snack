a320a9e1165c170080bd2fbc70c45af97223be85
Elisa Chiappini <elisabichiappini@gmail.com>
2025-01-14 12:03:19 +0100

    feat: funzione che data una stringa, conti le vocali al suo interno e ne ritorni il numero

--- snack.js
+++ snack.js
@@ -15,4 +15,22 @@ http.createServer((req, res,) => {
     res.end('<h1>ciao sono collegato</h1>');
 }).listen(port, host, () => {
     console.log(`server avviato su http://${host}:${port}`);
-})
+});
+
+//1.Creare una funzione che data una stringa, conti le vocali al suo interno e ne ritorni il numero
+const contaVocali = stringa => {
+    stringa = stringa.toLowerCase();
+    const vocali = "aeiou";
+
+    let contatore = 0;
+    for(let i = 0; i < stringa.length; i++) {
+        const carattere = stringa[i];
+        //se le tra vocali c'è questo carattere
+        if (vocali.includes(carattere)){
+            contatore++;
+        }
+    };
+    return contatore;
+}
+
+console.log(contaVocali('miao'));
